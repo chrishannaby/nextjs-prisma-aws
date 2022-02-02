767998f85cf1b24a5e3472a23decca9323416ea7
Chris Hannaby <chris.hannaby@netlify.com>
2022-02-02 15:28:17 -0700

    use ISR

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { GetServerSideProps } from "next";
+import { GetStaticProps, GetStaticPaths } from "next";
 import ReactMarkdown from "react-markdown";
 import Layout from "../../components/Layout";
 import { PostProps } from "../../components/Post";
 import prisma from "../../lib/prisma";
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
     where: {
       id: Number(params?.id) || -1,
